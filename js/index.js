f193288cee34ccd9274f639e6befba6856bcc3ad
SpaceStrix <3xiksy@gmail.com>
2022-07-08 23:58:47 +0300

    сделать бургер и анимацию

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ let field = document.querySelector('.form-search__field');
 function btnHandler(e) {
 	e.preventDefault();
 	field.classList.toggle('field-open');
+	field.classList.toggle('animated');
 }
 
 btn.addEventListener('click', btnHandler);
